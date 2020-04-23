55fad3f67e3add82dcfe8e207f9476652952af42
Vitor Pietrobom <v245584@dac.unicamp.com>
2020-04-22 21:12:29 -0300

    Sistema de upgrade aparentemente funcionando, faltam testes mais profundos

--- src/dama_package/Main.java
+++ src/dama_package/Main.java
@@ -3,7 +3,10 @@ package dama_package;
 public class Main {
     public static void main(String[] args){
         Tabuleiro tab=new Tabuleiro();
+        System.out.println("Estado inicial:");
         tab.print_tabuleiro();
+        System.out.println();
+        System.out.println();
         CSVReader csv = new CSVReader();
         csv.setDataSource("C:/Users/vitor/IdeaProjects/Dama_Project/src/dama_package/Data.csv");
         String commands[] = csv.requestCommands();
@@ -17,6 +20,18 @@ public class Main {
             if(vez == tab.matriz[atual[0]][atual[1]].equipe & tab.matriz[atual[0]][atual[1]] != null ){
                 Peca patual = tab.matriz[atual[0]][atual[1]];
                 patual.movimento(tab.matriz, atual, tab);
+                if (patual.equipe == 'B' && !patual.is_dama){
+                    if(atual[2] == 0){
+                        tab.upgrade('B',atual);
+                        patual.posso_continuar = 0;
+                    }
+                }
+                else if (patual.equipe == 'P' && !patual.is_dama){
+                    if(atual[2] == 7){
+                        tab.upgrade('P',atual);
+                        patual.posso_continuar = 0;
+                    }
+                }
                 patual = tab.matriz[atual[2]][atual[3]];
                 while(patual.posso_continuar == 1){
                     if(patual.verifica_captura(tab.matriz,atual)){
@@ -24,6 +39,18 @@ public class Main {
                         atual = tab.transformar_coordenadas(commands[i]);
                         patual = tab.matriz[atual[0]][atual[1]];
                         patual.movimento(tab.matriz, atual, tab);
+                        if (patual.equipe == 'B' && !patual.is_dama){
+                            if(atual[2] == 0){
+                                tab.upgrade('B',atual);
+                                patual.posso_continuar = 0;
+                            }
+                        }
+                        else if (patual.equipe == 'P' && !patual.is_dama){
+                            if(atual[2] == 7){
+                                tab.upgrade('P',atual);
+                                patual.posso_continuar = 0;
+                            }
+                        }
                     }
                     else{
                         patual.posso_continuar = 0;
